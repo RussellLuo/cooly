8a2a9c73e9c2ef48f592c4ba61e1ff41c84807c1
RussellLuo <luopeng.he@gmail.com>
2016-04-03 17:59:47 +0800

    Bump version to 0.1.3

--- cooly/__init__.py
+++ cooly/__init__.py
@@ -1,6 +1,6 @@
 """A utility that helps you deploy Python projects."""
 
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 __author__ = 'RussellLuo'
 __email__ = 'luopeng.he@gmail.com'
 __license__ = 'MIT'
